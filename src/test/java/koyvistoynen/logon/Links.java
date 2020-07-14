8e62ff69c0ef0e4a69a9c7eab0315fae15147fbf
koyvistoynen <denis@koyvistoynen.ru>
2020-07-15 01:20:58 +0300

    Добавлен класс для хранения ссылок

---
+++ src/test/java/koyvistoynen/logon/Links.java
@@ -0,0 +1,14 @@
+package koyvistoynen.logon;
+
+public class Links {
+    private String homePage = "https://koyvistoynen.ru";
+    public String getHomePage() {
+        return homePage;
+    }
+
+    private String authPage = homePage + "/admin";
+    public String getAuthPage() {
+        return authPage;
+    }
+
+}
