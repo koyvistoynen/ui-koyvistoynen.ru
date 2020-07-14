a02cd021414a9d0def13d9fc35e5def990b072db
koyvistoynen <denis@koyvistoynen.ru>
2020-07-15 01:21:49 +0300

    Добавлен класс с переменными для авторизации

---
+++ src/test/java/koyvistoynen/logon/Variables.java
@@ -0,0 +1,8 @@
+package koyvistoynen.logon;
+
+public class Variables {
+    String userEmailNegative = "exampleNegative@email.ru";
+    String userPasswordNegative = "exampleNegativePass";
+    String userEmailPositive = "examplePositive@email.ru";
+    String userPasswordPositive = "examplePositivePass";
+}
