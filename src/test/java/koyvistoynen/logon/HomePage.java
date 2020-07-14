0e0c5b507c060f7391555ed85f035e8e1f73c759
koyvistoynen <denis@koyvistoynen.ru>
2020-07-15 01:23:49 +0300

    Заменены захардкоженое значение на значение из переменных

--- src/test/java/koyvistoynen/logon/HomePage.java
+++ src/test/java/koyvistoynen/logon/HomePage.java
@@ -9,8 +9,8 @@ public class HomePage extends BasePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
-
-    String url = "https://koyvistoynen.ru";
+    Links links = new Links();
+    String url = links.getHomePage();
     By siteTitle = By.linkText("Admin Blog");
     By newEntries = By.xpath(".//*[text()='Свежие записи']/.");
     By rubric = By.xpath(".//*[text()='Рубрики']/.");
