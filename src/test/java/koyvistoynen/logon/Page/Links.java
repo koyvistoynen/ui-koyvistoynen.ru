ab3c875b8a1a2b226d2f613922a8ab8162729d7a
koyvistoynen <denis@koyvistoynen.ru>
2020-08-03 01:34:57 +0300

    Добавлены комментерии с описанием методов

--- src/test/java/koyvistoynen/logon/Page/Links.java
+++ src/test/java/koyvistoynen/logon/Page/Links.java
@@ -1,16 +1,20 @@
 package koyvistoynen.logon.Page;
 
 public class Links {
+
+    /**Домашняя страница"**/
     private String homePage = "https://koyvistoynen.ru";
     public String getHomePage() {
         return homePage;
     }
 
+    /**Страница авторизации"**/
     private String authPage = homePage + "/admin";
     public String getAuthPage() {
         return authPage;
     }
 
+    /**Страница поиска"**/
     private String searchPage = homePage + "/?s=";
     public String getSearchPage() {
         return searchPage;
